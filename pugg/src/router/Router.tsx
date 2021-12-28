f832a2232cbf6062535767f92bceaa5c2e898d30
paopao <1819027025@qq.com>
2021-12-28 15:43:42 +0800

    适配移动端

--- pugg/src/router/Router.tsx
+++ pugg/src/router/Router.tsx
@@ -9,7 +9,7 @@ import Home from "../containers/Home";
 export default function Router() {
     {/* 所有的路由配置均在 BrowserRouter 内部 */ }
     return (
-        <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+        <div style={{ width: '100%', height: '100vh' }}>
             <Header />
             <BrowserRouter>
                 {/* 使用 Routes 替换曾经的 Switch */}
